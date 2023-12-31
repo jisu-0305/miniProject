d1c077e52eb4b41ba1a471455c32d8d2bda3d096
kimjisu <comjisu0311@naver.com>
2023-12-31 23:06:18 +0900

    JPA Auditing 테스트 코드 추가

--- src/main/java/example/org/springboot/domain/posts/Posts.java
+++ src/main/java/example/org/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package example.org.springboot.domain.posts;
 
+import example.org.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
