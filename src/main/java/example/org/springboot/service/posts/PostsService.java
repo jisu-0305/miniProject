e6a535f5953c0c9336d53d7c5a5a71f8b650067c
kimjisu <comjisu0311@naver.com>
2024-01-02 15:26:14 +0900

    게시글 조회 목록 구현

--- src/main/java/example/org/springboot/service/posts/PostsService.java
+++ src/main/java/example/org/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package example.org.springboot.service.posts;
 
 import example.org.springboot.domain.posts.Posts;
 import example.org.springboot.domain.posts.PostsRepository;
+import example.org.springboot.web.dto.PostsListResponseDto;
 import example.org.springboot.web.dto.PostsResponseDto;
 import example.org.springboot.web.dto.PostsSaveRequestDto;
 import example.org.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
